8890f3c19b6f196cf05a7339a7a7bc354519fbdb
Thai Tran <thai.tommy.tran@gmail.com>
2022-10-02 20:32:02 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
+};
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        {/* Navbar */}
+        <Navbar />
+        {/* Chat component */}
+      </section>
     </div>
   );
 }
