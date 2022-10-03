da0751894d528c7c4869a2aa8b5c843d0051b55b
Thai Tran <thai.tommy.tran@gmail.com>
2022-10-02 23:05:45 -0500

    added LogOut component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SignIn from "./SignIn";
+import LogOut from "./LogOut";
 
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -12,11 +13,11 @@ const style = {
 const Navbar = () => {
   const [user] = useAuthState(auth);
   console.log(user);
-  
+
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat App</h1>
-      <SignIn />
+      {user ? <LogOut /> : <SignIn />}
     </div>
   );
 };
