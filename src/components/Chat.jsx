938fb9667a25915414bc65bd9e985950f0157ac7
Thai Tran <thai.tommy.tran@gmail.com>
2022-10-03 21:42:28 -0500

    created firebase db, added read from db functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,18 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
 
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+
 const style = {
   main: `flex flex-col p-[10px] relative`
 };
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
   const scroll = useRef();
+
+  // Read messages from firebase db
+  useEffect(() => {
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
       <main className={style.main}>
         {/* Chat Message Component */}
-        <Message />
+        {messages &&
+          messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
       </main>
       {/* Send Message Component */}
       <span ref={scroll}></span>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const style = {
-    message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full`,
-    name: `fixed mt-[-4rem] text-gray-600 text-xs`,
-    sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-    received: `bg-[#e5e5ea] text-black float-left rounded-br-full`
-}
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full`,
+  name: `fixed mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`
+};
 
-const Message = () => {
+const Message = ({ message }) => {
   return (
     <div>
-        <div className={style.message}>
-            <p className={style.name}>Thai</p>
-            <p>I'm Learning React!</p>
-        </div>
+      <div className={style.message}>
+        <p className={style.name}>Thai</p>
+        <p>{message.text}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
